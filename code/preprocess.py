35183ca87715df5de8492f055a2ee869b03cdb1e
greenare <greenare_@naver.com>
2022-03-31 01:10:21 +0000

    전처리 작업

--- code/preprocess.py
+++ code/preprocess.py
@@ -6,6 +6,7 @@ def wordlen(text):
     return len(text) < 400
 
 def Preprocess(df):
-    df_v1=df[df['sentence'].apply(wordlen)] # 400 글자 이상 제거
-    df_v2=df_v1.drop_duplicates(['subj_word','sentence','obj_word','subj_start','obj_start','label'],keep='first') # word,sen,idx,label 중복된 경우 제거
+    # df_v1=df[df['sentence'].apply(wordlen)] # 400 글자 이상 제거
+    df_v2=df.drop_duplicates(['subj_word','sentence','obj_word','subj_start','obj_start','label'],keep='first') # word,sen,idx,label 중복된 경우 제거
     df_v3 = df_v2.drop([6749,8364,22258,277,25094]) # word,sen,idx가 동일하고, label만 다른 경우 이상 label data 제거
+    return df_v3
